c573aa79156d75bca4768c7cd097c5109bde24d1
Murugan Latha <rm24aao@herts.ac.uk>
2024-11-24 19:45:53 +0000

    #Update: fixing the X (filled the empty space) and Y (price in millon) axis

--- R script/R script.R
+++ R script/R script.R
@@ -16,3 +16,29 @@ head(selected_columns)  # Display the first few rows
    grid()
    
    
+   
+   dataset <- read.csv("melb_data.csv")
+   
+   
+   plot(dataset$Rooms, dataset$Price,
+        main = "Scatterplot of Number of Rooms vs. Price",
+        xlab = "Number of Rooms",
+        ylab = "Price (in AUD Millions)",
+        pch = 19,          
+        col = rgb(0, 0, 1, 0.5), 
+        xlim = c(1, 10),   
+        xaxt = "n",        
+        yaxt = "n")       
+   
+   
+   grid()
+   
+   
+   axis(1, at = 1:10, labels = 1:10)
+   
+   
+   max_price <- max(dataset$Price, na.rm = TRUE)
+   y_ticks <- seq(0, max_price, by = 1e6)  
+   axis(2, at = y_ticks, labels = y_ticks / 1e6, las = 2)  
+   
+   
