4f77037d6982a6891bdbd8be84858a2874231312
Sibiantony Sowrirajan <asibi8593@gmail.com>
2024-11-25 00:37:38 +0530

    #Update: Scatter plot visualization for Rooms vs Price.

--- R script/R script.R
+++ R script/R script.R
@@ -1,2 +1,18 @@
 selected_columns <- dataset[, c("Suburb", "Address", "Rooms", "Type", "Price")]
 head(selected_columns)  # Display the first few rows
+
+# Load the dataset
+ dataset <- read.csv("melb_data.csv")
+ 
+   # Scatterplot of Rooms vs. Price
+   plot(dataset$Rooms, dataset$Price,
+               main = "Scatterplot of Number of Rooms vs. Price",
+               xlab = "Number of Rooms",
+               ylab = "Price (AUD)",
+               pch = 19,        # Solid circles
+               col = rgb(0, 0, 1, 0.5)) # Blue with transparency
+
+  # Adding gridlines
+   grid()
+   
+   
